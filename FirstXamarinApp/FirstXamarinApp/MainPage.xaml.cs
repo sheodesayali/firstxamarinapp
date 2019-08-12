63f6bef4ecb892206e8ccaffa7f80e7c660186d1
sayalisheode <sayali.p.sheode@gmail.com>
2019-08-12 08:40:16 -0700

    Add project files.

---
+++ FirstXamarinApp/FirstXamarinApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstXamarinApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
