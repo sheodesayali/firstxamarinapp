def42080f5f5d20ac80a5f44f8aa609297385796
Sayali Sheode <sayali.sheode@pelesys.com>
2019-08-12 10:22:52 -0700

    Clean and Rebuild App

--- SampleUITests/AppInitializer.cs
+++ SampleUITests/AppInitializer.cs
@@ -10,7 +10,7 @@ namespace SampleUITests
         {
             if (platform == Platform.Android)
             {
-                return ConfigureApp.Android.StartApp();
+                return ConfigureApp.Android.ApkFile("C:/Users/Sayali.Sheode/Desktop/com.companyname.firstxamarinapp.apk").StartApp();
             }
 
             return ConfigureApp.iOS.StartApp();
