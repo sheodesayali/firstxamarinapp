b2d5c4a95d2fac6e8987479076fbebf5ad24933d
Sayali Sheode <sayali.sheode@pelesys.com>
2019-08-15 14:12:02 -0700

    Add Repl

--- SampleUITests/Tests.cs
+++ SampleUITests/Tests.cs
@@ -22,16 +22,26 @@ namespace SampleUITests
         [SetUp]
         public void BeforeEachTest()
         {
-            app = AppInitializer.StartApp(platform);
+            app = ConfigureApp.Android.StartApp();
+            //app = AppInitializer.StartApp(platform);
+            app.Repl();
         }
 
         [Test]
         public void WelcomeTextIsDisplayed()
         {
+            app.Repl();
             AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
             app.Screenshot("Welcome screen.");
 
             Assert.IsTrue(results.Any());
         }
+
+        [Test]
+        public void TwoClicks()
+        {
+            app.Repl();
+        }
+
     }
 }
